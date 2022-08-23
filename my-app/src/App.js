61784360f25f9b35b36bfc0fed8751ff44a8fbf2
Alex Canez <el.winy.36@gmail.com>
2022-08-22 21:07:17 -0600

    added more css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import instaLogo from  './Assets/Pics/Screenshot-logo.png';
+import instaLogo from './Assets/Pics/Screenshot-logo.png';
 import neonLogo from './Assets/Pics/blacklogocircle.png';
 import { useState } from 'react';
 import Contact from './Components/Pages/Contact';
@@ -12,8 +12,8 @@ function App() {
   return (
     <div>
       <nav>
-        <ul style={{ display: 'flex', listStyle: 'none'}}>
-        <a target='_blank' href="https://www.instagram.com/neon_house_signs/?hl=en"><img border="1" alt="Insta" src={neonLogo} width="60" height="60"></img></a>
+        <ul style={{ display: 'flex', listStyle: 'none' }}>
+          <a target='_blank' href="https://www.instagram.com/neon_house_signs/?hl=en"><img border="1" alt="Insta" src={neonLogo} width="60" height="60"></img></a>
           <li>Neon House Signs</li>
           <li onClick={() => setcurrentPage("Aboutus")}>About Us</li>
           <li onClick={() => setcurrentPage("Signs")}>Signs</li>
@@ -23,12 +23,15 @@ function App() {
       </nav>
       {currentPage === "Aboutus" ? <Aboutus />
         : currentPage === "Signs" ? <Signs />
-            : <Contact />}
+          : <Contact />}
 
       <footer>
         <div class='row'>
-        <div class="column">
-            <h4>Follow Us:</h4>
+          <div class="column">
+            <h4>Any questions or concerns?
+              Let us know and we'll be happy to talk to you!
+              P:801.830.2981
+              E:NEONHOUSESIGNS@GMAIL.COM</h4>
           </div>
           <div class='footerlogo'>
             <a target='_blank' href="https://www.instagram.com/neon_house_signs/?hl=en"><img border="1" alt="Insta" src={instaLogo} width="80" height="80"></img></a>
