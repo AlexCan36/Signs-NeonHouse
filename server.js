e4d0c5e4aeede4fd024f20ec391ac880f6d59556
Alex Canez <el.winy.36@gmail.com>
2022-08-23 20:59:59 -0600

    added heroku

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+const express = require('express');
+
+const app = express();
+
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
   });
