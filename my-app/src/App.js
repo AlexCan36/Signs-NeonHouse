87b8dd51d81e1c92b625957274509d255e2fb84c
Alex Canez <el.winy.36@gmail.com>
2022-08-23 20:45:08 -0600

    added heroku

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import instaLogo from './Assets/Pics/Screenshot-logo.png';
 import neonLogo from './Assets/Pics/blacklogocircle.png';
--- my-app/src/Components/Pages/Contact.js
+++ my-app/src/Components/Pages/Contact.js
@@ -1,4 +1,4 @@
-import Header from "../Header";
+//import Header from "../Header";
 import React, { useState } from 'react';
 
 import { validateEmail } from '../../utils/helpers';
